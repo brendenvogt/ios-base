547e03d16cb2ef28e26d01f63b244d7021439429
BrendenVogt <bvogtdev@gmail.com>
2018-09-24 21:09:31 -0700

    initial commit

--- PROJNAME/AppDelegate.swift
+++ PROJNAME/AppDelegate.swift
@@ -16,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let homeViewController = HomeViewController()
+        homeViewController.view.backgroundColor = UIColor.red
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
+
         return true
     }
 
---
+++ PROJNAME/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  PROJNAME
+//
+//  Created by Brenden Vogt on 9/24/18.
+//  Copyright © 2018 ORGNAME. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello from home view controller")
+    }
+
+}
+
