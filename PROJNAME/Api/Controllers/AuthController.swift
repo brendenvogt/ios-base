7181d19c3a9d411e9d5d32b3fea153cffd405ef9
BrendenVogt <bvogtdev@gmail.com>
2018-10-02 05:16:42 -0700

    refactor

--- PROJNAME/Api/Controllers/AuthController.swift
+++ PROJNAME/Api/Controllers/AuthController.swift
@@ -11,29 +11,4 @@ import Alamofire
 
 class AuthController: NSObject {
     
-    public class func getApi(completion: @escaping (BoolContract?, ErrorContract?) -> Void) {
-        Alamofire.request(UrlFactory.api()).responseJSON { response in
-            if let data = response.data {
-                let error: ErrorContract? = try? JSONDecoder().decode(ErrorContract.self, from: data)
-                let result: BoolContract? = try? JSONDecoder().decode(BoolContract.self, from: data)
-                completion(result, error)
-            }
-        }
-    }
-    
-    static func getAuthFromJson(json : String) -> AuthRequestContract? {
-        if let decoded = try? JSONDecoder().decode(AuthRequestContract.self, from: json.data(using: .utf8)!) {
-            return decoded
-        }
-        return nil
-    }
-    
-    static func getAuth(credentials : AuthRequestContract) -> AuthRequestContract? {
-        if let encoded = try? JSONEncoder().encode(credentials){
-            if let decoded = try? JSONDecoder().decode(AuthRequestContract.self, from: encoded) {
-                return decoded
-            }
-        }
-        return nil
-    }
 }
