3b79ea86dfaa023c954fee92c70d2aeaccafef79
BrendenVogt <bvogtdev@gmail.com>
2018-10-28 08:37:28 -0700

    added images and extension

--- PROJNAME/Components/Extensions/UIImageExtension.swift
+++ PROJNAME/Components/Extensions/UIImageExtension.swift
@@ -99,4 +99,25 @@ extension UIImage{
         return (primaryColor, secondaryColor, detailColor)
     }
     
+    /// Creates a circular outline image.
+    class func outlinedEllipse(size: CGSize, color: UIColor, lineWidth: CGFloat = 1.0) -> UIImage? {
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        
+        context.setStrokeColor(color.cgColor)
+        context.setLineWidth(lineWidth)
+        // Inset the rect to account for the fact that strokes are
+        // centred on the bounds of the shape.
+        let rect = CGRect(origin: .zero, size: size).insetBy(dx: lineWidth * 0.5, dy: lineWidth * 0.5)
+        context.addEllipse(in: rect)
+        context.strokePath()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+
 }
